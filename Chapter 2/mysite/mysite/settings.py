faf2a1638f44e93f614045ab226d3da8f1a1dc1f
Tehmeer Ali Paryani <freelanceali786@gmail.com>
2019-03-31 21:39:54 +0500

    Chapter two comments form.

--- Chapter 2/mysite/mysite/settings.py
+++ Chapter 2/mysite/mysite/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'freelanceali786@gmail.com'
+EMAIL_HOST_PASSWORD = 'YaProtectorYaMerciful786()'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True

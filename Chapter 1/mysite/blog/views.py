f11f562dda96960ffff7b0c826ff69dd24b2f461
Tehmeer Ali Paryani <freelanceali786@gmail.com>
2019-03-31 08:36:11 +0500

    Chapter 1

--- Chapter 1/mysite/blog/views.py
+++ Chapter 1/mysite/blog/views.py
@@ -33,4 +33,4 @@ class PostListView(ListView):
 
 def post_detail(request, year, month, day, post):
 	post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
-	return render(request, 'blog/post/detail.html', { 'page': page, 'post' : post})
+	return render(request, 'blog/post/detail.html', { 'post' : post})

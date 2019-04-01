4a291139797ae6ed45b10abbda93dc1bb1fd99f6
Tehmeer Ali Paryani <freelanceali786@gmail.com>
2019-04-01 21:50:01 +0500

    Tag list

--- Chapter 2/mysite/blog/views.py
+++ Chapter 2/mysite/blog/views.py
@@ -5,11 +5,23 @@ from django.views.generic import ListView
 
 from django.core.mail import send_mail
 from .forms import EmailPostForm, CommentForm
+from taggit.models import Tag
 
 # Create your views here.
-'''
-def post_list(request):
+
+def post_list(request, tag_slug=None):
 	object_list = Post.published.all()
+
+	tag=None 
+
+	if tag_slug:
+
+		tag = get_object_or_404(Tag, slug=tag_slug)
+		object_list = object_list.filter(tags__in=[tag])
+
+
+
+
 	paginator = Paginator(object_list, 3)
 	page = request.GET.get('page')
 	try:
@@ -23,15 +35,17 @@ def post_list(request):
 		# If page is out of range deliver last page of results
 		posts = paginator.page(paginator.num_pages)
 
-	return render(request, 'blog/post/list.html', {'posts' : posts})
+	return render(request, 'blog/post/list.html', {'page': page, 'posts' : posts, 'tag' : tag})
 
-	'''
-
+	
+'''
 class PostListView(ListView):
 	queryset = Post.published.all()
 	context_object_name = 'posts'
 	paginate_by = 3
 	template_name = 'blog/post/list.html'
+
+'''
 
 
 def post_detail(request, year, month, day, post):
